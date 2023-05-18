9d8cfdfc4d798ea16686d6ba24ed93755ebb1e0e
Shaoor <shaooriqbal786@gmail.com>
2023-05-18 16:38:04 +0500

    Node js app crud

---
+++ db.js
@@ -0,0 +1,16 @@
+const { error } = require('console');
+const { MongoClient } = require('mongodb');
+let dbConnection;
+module.exports = {
+    connectToDb: (cb) => {
+        MongoClient.connect('mongodb://localhost:27017/cakes-database').then((client) => {
+            dbConnection = client.db();
+            return cb();
+        }).catch(error => {
+            console.log(error);
+            return cb(error);
+        });
+    },
+    getDb: () => dbConnection
+
+}
