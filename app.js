5ff2bc334b80cedfdc1251f57331539f879ad35f
Shaoor <shaooriqbal786@gmail.com>
2023-05-24 13:02:30 +0500

    strctural change

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const { connectToDb, getDb } = require('./db');
 const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
-const User = require('./models/user');
 const userRoutes = require('./routes/userRoutes');
-
+const localRoutes = require('./routes/localDbRoutes');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRoutes);
+app.use(localRoutes);
 let db;
 
 //mongoose connection
@@ -24,77 +24,4 @@ mongoose.connect(dbUri).then(() => app.listen(3000, () => {
 //         db = getDb();
 //     }
 // });
-
-//get all cakes
-app.get('/cakes', (req, res) => {
-    const page = req.query.p || 0;
-    const limit = 5;
-    let cakes = [];
-    db.collection('cakes').find().skip(page * limit).limit(limit).forEach(cake =>
-        cakes.push(cake)
-    ).then(() => {
-        res.status(200).json(cakes);
-    }).catch(() => {
-        res.status(500).json({ error: "Fetching error" })
-    })
-
-});
-
-// get single cake
-app.get('/cakes/:id', (req, res) => {
-    let id = req.params.id;
-    if (ObjectId.isValid(id)) {
-        db.collection('cakes').findOne({ _id: new ObjectId(id) }).then(doc => {
-            res.status(200).json(doc);
-        }).catch(err => { res.status(500).json(err) });
-    } else {
-        res.status(500).json({ error: "Id given is not valid" });
-    }
-});
-
-// post request
-
-app.post('/cakes', (req, res) => {
-    const cake = req.body;
-
-    db.collection('cakes').insertOne(cake).then(result => {
-        res.status(201).json(result)
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    });
-});
-
-//Delete request
-
-app.delete('/cakes/:id', (req, res) => {
-    let id = req.params.id;
-    if (ObjectId.isValid(id)) {
-        db.collection('cakes').deleteOne({ _id: new ObjectId(id) }).then(result => {
-            res.status(200).json(result)
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json(err)
-        });
-    } else {
-        res.status(500).json({ error: "Id given is not valid" });
-    }
-});
-
-// update request
-
-app.patch('/cakes/:id', (req, res) => {
-    const updates = req.body;
-    let id = req.params.id;
-    if (ObjectId.isValid(id)) {
-        db.collection('cakes').updateOne({ _id: new ObjectId(id) }, { $set: updates }).then(result => {
-            res.status(200).json(result)
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json(err)
-        });
-    } else {
-        res.status(500).json({ error: "Id given is not valid" });
-    }
-});
 
---
+++ routes/localDbRoutes.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+
+//get all cakes
+router.get('/cakes', (req, res) => {
+    const page = req.query.p || 0;
+    const limit = 5;
+    let cakes = [];
+    db.collection('cakes').find().skip(page * limit).limit(limit).forEach(cake =>
+        cakes.push(cake)
+    ).then(() => {
+        res.status(200).json(cakes);
+    }).catch(() => {
+        res.status(500).json({ error: "Fetching error" })
+    })
+
+});
+
+// get single cake
+router.get('/cakes/:id', (req, res) => {
+    let id = req.params.id;
+    if (ObjectId.isValid(id)) {
+        db.collection('cakes').findOne({ _id: new ObjectId(id) }).then(doc => {
+            res.status(200).json(doc);
+        }).catch(err => { res.status(500).json(err) });
+    } else {
+        res.status(500).json({ error: "Id given is not valid" });
+    }
+});
+
+// post request
+
+router.post('/cakes', (req, res) => {
+    const cake = req.body;
+
+    db.collection('cakes').insertOne(cake).then(result => {
+        res.status(201).json(result)
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+//Delete request
+
+router.delete('/cakes/:id', (req, res) => {
+    let id = req.params.id;
+    if (ObjectId.isValid(id)) {
+        db.collection('cakes').deleteOne({ _id: new ObjectId(id) }).then(result => {
+            res.status(200).json(result)
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    } else {
+        res.status(500).json({ error: "Id given is not valid" });
+    }
+});
+
+// update request
+
+router.patch('/cakes/:id', (req, res) => {
+    const updates = req.body;
+    let id = req.params.id;
+    if (ObjectId.isValid(id)) {
+        db.collection('cakes').updateOne({ _id: new ObjectId(id) }, { $set: updates }).then(result => {
+            res.status(200).json(result)
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    } else {
+        res.status(500).json({ error: "Id given is not valid" });
+    }
+});
